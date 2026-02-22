9320ca3bf29a8ee9f5bddaa7bae0d9830a30f627
Antonio Marasescu-Duran <antonio.marasescuduran@gmail.com>
2026-02-22 22:29:29 +0200

    fix: eslint issues

--- src/data/class-data.ts
+++ src/data/class-data.ts
@@ -1,11 +1,11 @@
-import { beastheart } from "@/data/classes/beastheart/beastheart.ts";
+import { beastheart } from '@/data/classes/beastheart/beastheart.ts';
 import { censor } from '@/data/classes/censor/censor';
 import { conduit } from '@/data/classes/conduit/conduit';
 import { elementalist } from '@/data/classes/elementalist/elementalist';
 import { fury } from '@/data/classes/fury/fury';
 import { nullClass } from '@/data/classes/null/null';
 import { shadow } from '@/data/classes/shadow/shadow';
-import { summoner } from "@/data/classes/summoner/summoner.ts";
+import { summoner } from '@/data/classes/summoner/summoner.ts';
 import { tactician } from '@/data/classes/tactician/tactician';
 import { talent } from '@/data/classes/talent/talent';
 import { troubadour } from '@/data/classes/troubadour/troubadour';
